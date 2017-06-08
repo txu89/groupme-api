000f494ea7f8654e14e20ca59f36578e49c3d2ba
Tom Xu <xudotcom@gmail.com>
2017-06-08 01:25:17 -0400

    fixed bug

--- app/index.js
+++ app/index.js
@@ -5,7 +5,8 @@ var SendMessage = React.createClass({
   storedMessage: "",
   getInitialState: function () {
     return {
-      message: ""
+      message: "",
+      value: "test"
     }
   },
   saveMessage: function () {
@@ -16,29 +17,35 @@ var SendMessage = React.createClass({
     this.postMessage()
   },
   postMessage: function () {
-    $.ajax({
-      headers: {
-        "X-Access-Token": "BGil3vVRxa89yPYS30uEVochMQ32vKDy4PRcQRxs"
-      },
-      method: 'POST',
-      url: "https://api.groupme.com/v3/groups/31465394/messages",
-      dataType: 'json',
-      contentType: "application/json",
-      data: JSON.stringify({
-        "message": {
-          "source_guid": Math.floor((Math.random() *1000000) +1),
-          "text": this.storedMessage,
-        }
-      }),
-      success: function(results){
-        var str = JSON.stringify(results, null, 2);
-        $(".display").text(str)
-        console.log(results)
-      },
-      error: function() {
-        console.log(this.url)
-      }
-    })
+    // $.ajax({
+    //   headers: {
+    //     "X-Access-Token": "BGil3vVRxa89yPYS30uEVochMQ32vKDy4PRcQRxs"
+    //   },
+    //   method: 'POST',
+    //   url: "https://api.groupme.com/v3/groups/31465394/messages",
+    //   dataType: 'json',
+    //   contentType: "application/json",
+    //   data: JSON.stringify({
+    //     "message": {
+    //       "source_guid": Math.floor((Math.random() *1000000) +1),
+    //       "text": this.storedMessage,
+    //     }
+    //   }),
+    //   success: function(results){
+    //     var str = JSON.stringify(results, null, 2);
+    //     $(".display").text(str)
+    //     console.log(results)
+    //   },
+    //   error: function() {
+    //     console.log(this.url)
+    //   }
+    // })
+    console.log('post message', this.state)
+  },
+  handleChange: function(event) {
+    console.log(event)
+    console.log(this.state.value)
+    this.setState({value: event.target.value})
   },
   render: function () {
     console.log(this.state.message)
@@ -46,6 +53,7 @@ var SendMessage = React.createClass({
       <div>
         enter message:
         <textarea rows="4" cols="50" id="message"></textarea>
+        <input type="text" value={this.state.value} onChange={this.handleChange}></input>
         <button onClick={this.saveMessage}>
           send </button>
         <h3>{this.state.message}</h3>
