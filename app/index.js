4498d680ec4187d0393a287cd5c052481289b1fa
Tom Xu <xudotcom@gmail.com>
2017-06-08 01:35:39 -0400

    used controlled form input to save text message

--- app/index.js
+++ app/index.js
@@ -2,59 +2,48 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 
 var SendMessage = React.createClass({
-  storedMessage: "",
   getInitialState: function () {
     return {
       message: "",
-      value: "test"
     }
   },
-  saveMessage: function () {
-    this.storedMessage = document.getElementById('message').value
-    this.setState({
-      message: this.storedMessage
-    });
-    this.postMessage()
-  },
   postMessage: function () {
-    // $.ajax({
-    //   headers: {
-    //     "X-Access-Token": "BGil3vVRxa89yPYS30uEVochMQ32vKDy4PRcQRxs"
-    //   },
-    //   method: 'POST',
-    //   url: "https://api.groupme.com/v3/groups/31465394/messages",
-    //   dataType: 'json',
-    //   contentType: "application/json",
-    //   data: JSON.stringify({
-    //     "message": {
-    //       "source_guid": Math.floor((Math.random() *1000000) +1),
-    //       "text": this.storedMessage,
-    //     }
-    //   }),
-    //   success: function(results){
-    //     var str = JSON.stringify(results, null, 2);
-    //     $(".display").text(str)
-    //     console.log(results)
-    //   },
-    //   error: function() {
-    //     console.log(this.url)
-    //   }
-    // })
+    $.ajax({
+      headers: {
+        "X-Access-Token": "BGil3vVRxa89yPYS30uEVochMQ32vKDy4PRcQRxs"
+      },
+      method: 'POST',
+      url: "https://api.groupme.com/v3/groups/11518960/messages",
+      dataType: 'json',
+      contentType: "application/json",
+      data: JSON.stringify({
+        "message": {
+          "source_guid": Math.floor((Math.random() *1000000) +1),
+          "text": this.state.message,
+        }
+      }),
+      success: function(results){
+        var str = JSON.stringify(results, null, 2);
+        $(".display").text(str)
+        console.log(results)
+      },
+      error: function() {
+        console.log(this.url)
+      }
+    })
     console.log('post message', this.state)
   },
   handleChange: function(event) {
-    console.log(event)
-    console.log(this.state.value)
-    this.setState({value: event.target.value})
+    console.log(this.state.message)
+    this.setState({message: event.target.value})
   },
   render: function () {
-    console.log(this.state.message)
     return (
       <div>
         enter message:
-        <textarea rows="4" cols="50" id="message"></textarea>
-        <input type="text" value={this.state.value} onChange={this.handleChange}></input>
-        <button onClick={this.saveMessage}>
+        <textarea rows="4" cols="50" value={this.state.message}
+          onChange={this.handleChange}></textarea>
+        <button onClick={this.postMessage}>
           send </button>
         <h3>{this.state.message}</h3>
         <p className="display"></p>
